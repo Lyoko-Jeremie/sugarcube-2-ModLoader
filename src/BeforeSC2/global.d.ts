d2c87e1aa7a021870454aca584ffabb4a9af81d3
LyokoJeremie <lucheng989898@gmail.com>
2023-09-24 22:44:01 +0800

    global

--- src/BeforeSC2/global.d.ts
+++ src/BeforeSC2/global.d.ts
@@ -3,5 +3,12 @@ import type JQueryStatic from 'jquery/JQueryStatic';
 declare global {
     interface Window {
         jQuery: JQueryStatic;
+
+        modSC2DataManager: SC2DataManager;
+        modUtils: ModUtils;
+        jsPreloader: JsPreloader;
+        modModLoadController: ModLoadController;
+        modAddonPluginManager: AddonPluginManager;
+        modSC2JsEvalContext: SC2JsEvalContext;
     }
 }
