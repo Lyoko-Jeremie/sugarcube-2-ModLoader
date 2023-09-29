accb20cf1b39cc8a626b115297a38ad88dbb69c9
LyokoJeremie <lucheng989898@gmail.com>
2023-09-29 23:11:42 +0800

    JqueryInjector

--- src/BeforeSC2/JqueryInjector.ts
+++ src/BeforeSC2/JqueryInjector.ts
@@ -5,9 +5,13 @@ export function Inject4Jquery(thisWin: Window) {
     thisWin.jQuery = jq;
 
     thisWin.jQuery(async () => {
-        console.log('JqueryInjector ============= startInit()');
-        await thisWin.modSC2DataManager.startInit();
-        console.log('JqueryInjector ============= startLoad()');
-        await thisWin.jsPreloader.startLoad();
+        try {
+            console.log('JqueryInjector ============= startInit()');
+            await thisWin.modSC2DataManager.startInit();
+            console.log('JqueryInjector ============= startLoad()');
+            await thisWin.jsPreloader.startLoad();
+        } catch (e) {
+            console.error('Inject4Jquery', e);
+        }
     });
 }
