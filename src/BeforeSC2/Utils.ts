fe4f47906082c5f2865817124d22d636f6dc82e8
LyokoJeremie <lucheng989898@gmail.com>
2023-10-11 09:03:34 +0800

    1.2.3

--- src/BeforeSC2/Utils.ts
+++ src/BeforeSC2/Utils.ts
@@ -8,7 +8,7 @@ import {replaceMergeSC2DataInfoCache, replaceMergeSC2DataInfoCacheForce} from ".
 
 export class ModUtils {
 
-    version = '1.2.2';
+    version = '1.2.3';
 
     constructor(
         public pSC2DataManager: SC2DataManager,
