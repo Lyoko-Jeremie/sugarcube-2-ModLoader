f612d38f6ca857e3cd3ae859b85e4c8f55d47272
LyokoJeremie <lucheng989898@gmail.com>
2023-10-18 03:19:40 +0800

    1.6.0 now we can hook the html img tag create by SC2

--- src/BeforeSC2/Utils.ts
+++ src/BeforeSC2/Utils.ts
@@ -8,7 +8,7 @@ import {replaceMergeSC2DataInfoCache, replaceMergeSC2DataInfoCacheForce} from ".
 
 export class ModUtils {
 
-    version = '1.5.3';
+    version = '1.6.0';
 
     constructor(
         public pSC2DataManager: SC2DataManager,
