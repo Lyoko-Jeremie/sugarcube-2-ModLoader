86596894c05010381ad47cf44c9e3fafee97aae8
LyokoJeremie <lucheng989898@gmail.com>
2023-11-17 16:09:07 +0800

    2.6.1

--- src/BeforeSC2/Utils.ts
+++ src/BeforeSC2/Utils.ts
@@ -25,7 +25,7 @@ import {SemVerToolsType} from "./SemVer/InfiniteSemVer";
 export class ModUtils {
 
     get version(): string {
-        return '2.6.0';
+        return '2.6.1';
     }
 
     constructor(
