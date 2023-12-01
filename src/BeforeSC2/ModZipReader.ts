83c9d041a57a93b1121d9a05b21b2415835b9186
LyokoJeremie <lucheng989898@gmail.com>
2023-12-01 14:54:27 +0800

    Twee2Passage allow empty []

--- src/BeforeSC2/ModZipReader.ts
+++ src/BeforeSC2/ModZipReader.ts
@@ -12,16 +12,19 @@ export interface Twee2PassageR {
     name: string;
     tags: string[];
     contect: string;
-};
+}
 
 export function Twee2Passage(s: string): Twee2PassageR[] {
     // match:
     //      :: Widgets Bodywriting Objects [widget]
     //      :: Widgets Bodywriting Objects
     //      :: Widgets Bodywriting Objects [widget asdasd]
-    const r = s.split(/^(:: +((?:[^:"\\/\n\r\[\] ]+ *)+)(?:(?: +\[((?:\w+ *)+)\] *)?|))$/gm);
+    // special allow :
+    //      :: Widgets Bodywriting Objects []
+    const r = s.split(/^(:: +((?:[^:"\\/\n\r\[\] ]+ *)+)(?: +\[((?:\w+ *)+)?\] *|))$/gm);
     // console.log('Twee2Passage split ', r, [s]);
-    // ['xxx', ':: Widgets Bodywriting Objects [widget]', 'Widgets', 'Bodywriting Objects', 'widget', 'xxx']
+    // ['xxx', ':: Widgets Bodywriting Objects [widget]', 'Widgets Bodywriting Objects', 'widget', 'xxx']
+    // ['xxx', ':: Widgets Bodywriting Objects [widget]', 'Widgets Bodywriting Objects', undefined, 'xxx']
     const rr: Twee2PassageR[] = [];
     for (let i = 0; i < r.length; i++) {
         if (r[i].startsWith(':: ')) {
