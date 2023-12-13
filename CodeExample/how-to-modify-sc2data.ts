6c3964354ad03a43013d9957cce967d954a61c59
LyokoJeremie <lucheng989898@gmail.com>
2023-12-13 21:02:18 +0800

    CodeExample

---
+++ CodeExample/how-to-modify-sc2data.ts
@@ -0,0 +1,35 @@
+import type {} from '../src/BeforeSC2/global';
+import type {SC2DataManager} from '../dist-BeforeSC2/SC2DataManager';
+import type {ModUtils} from '../dist-BeforeSC2/Utils';
+import type {SC2DataInfoCache, PassageDataItem} from "../dist-BeforeSC2/SC2DataInfoCache";
+import {SC2DataInfo} from "../src/BeforeSC2/SC2DataInfoCache";
+
+const gUtils: ModUtils = window.modUtils;
+const gSC2DataManager: SC2DataManager = window.modSC2DataManager;
+
+const scOld: SC2DataInfoCache = gSC2DataManager.getSC2DataInfoAfterPatch();
+// make a new copy of sc2data, keep the old one.
+// notice: never touch the `scOld`, the `gUtils.replaceFollowSC2DataInfo` need the `scOld` to speed up replace
+const sc: SC2DataInfo = scOld.cloneSC2DataInfo();
+
+{
+    // do something...
+
+    // get the passage by name
+    const pp: PassageDataItem | undefined = sc.passageDataItems.map.get('PassageName');
+    // the passage name:
+    console.log(`passageName:[${pp.name}]`);
+
+    // change the passage code
+    // note that , the code not include the passage name `:: PassageName` line
+    pp.content.replace('aaaa', 'bbbb');
+
+    // similar like the passage, you can modify the js code
+    sc.scriptFileItems.map.get('scriptName.js')?.content.replace('aaaa', 'bbbb');
+}
+
+// if you modify the `passageDataItems.map`, you need call this to update the `passageDataItems.items`
+// because the `gUtils.replaceFollowSC2DataInfo` use the `passageDataItems.items` to update the html
+sc.passageDataItems.back2Array();
+gUtils.replaceFollowSC2DataInfo(sc, scOld);
+
