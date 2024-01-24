d82e79dc0ae1d7bf2eea640e73e6356fe74f511d
LyokoJeremie <lucheng989898@gmail.com>
2024-01-24 14:56:31 +0800

    comment

--- src/BeforeSC2/WikifyTracer.ts
+++ src/BeforeSC2/WikifyTracer.ts
@@ -4,11 +4,45 @@ import {LogWrapper} from "./ModLoadController";
 
 // all must sync impl, and must fastest
 export interface WikifyTracerCallback {
+    /**
+     * 可以修改输入文本内容
+     * @param text              原始文本内容
+     * @param passageTitle      passage标题
+     * @param passageObj        passage对象
+     * @return string           返回修改后的文本内容
+     */
     beforePassage?: (text: string, passageTitle: string, passageObj: SC2Passage) => string;
+    /**
+     * 可以修改输出后的html树，只会在passage结束后调用
+     * @param text          原始文本内容
+     * @param passageTitle  passage标题
+     * @param passageObj    passage对象
+     * @param node          最终输出的html树
+     */
     afterPassage?: (text: string, passageTitle: string, passageObj: SC2Passage, node: DocumentFragment) => void;
     beforeWikify?: (text: string) => string;
+    /**
+     * 可以修改输出后的html树，会在每一次wikify后调用，包括passage以及每个text节点
+     * @param text      原始文本内容
+     * @param node      最终输出的html树
+     */
     afterWikify?: (text: string, node: DocumentFragment) => void;
+    /**
+     * 可以修改输入文本内容
+     * @param text              原始文本内容
+     * @param widgetName        widget名称
+     * @param passageTitle      passage标题
+     * @param passageObj        passage对象
+     */
     beforeWidget?: (text: string, widgetName: string, passageTitle?: string, passageObj?: SC2Passage) => string;
+    /**
+     * 可以修改输出后的html树，会在每一次widget后调用
+     * @param text              原始文本内容
+     * @param widgetName        widget名称
+     * @param passageTitle      passage标题
+     * @param passageObj        passage对象
+     * @param node              最终输出的html树
+     */
     afterWidget?: (text: string, widgetName: string, passageTitle: string | undefined, passageObj: SC2Passage | undefined, node: DocumentFragment) => void;
 }
 
