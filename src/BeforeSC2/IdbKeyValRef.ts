1a50a3eec8390f1181f506c07ac9c50ff56a3850
LyokoJeremie <lucheng989898@gmail.com>
2024-10-27 18:16:57 +0800

    IdbRef

--- src/BeforeSC2/IdbKeyValRef.ts
+++ src/BeforeSC2/IdbKeyValRef.ts
@@ -2,11 +2,11 @@ import {get as keyval_get, set as keyval_set, del as keyval_del, createStore, Us
 import {openDB as idb_openDB, deleteDB as idb_deleteDB} from 'idb';
 
 export class IdbRef {
-    get idb_openDB() {
+    get idb_openDB(): typeof idb_openDB {
         return idb_openDB;
     }
 
-    get idb_deleteDB() {
+    get idb_deleteDB(): typeof idb_deleteDB {
         return idb_deleteDB;
     }
 
