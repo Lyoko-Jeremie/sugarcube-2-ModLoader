4caace33973956983c58ded739d9fe554b5f8b5e
LyokoJeremie <lucheng989898@gmail.com>
2025-05-18 18:44:22 +0800

    2.33.2

--- src/BeforeSC2/Utils.ts
+++ src/BeforeSC2/Utils.ts
@@ -27,7 +27,7 @@ import {ModLoadFromSourceType} from "./ModOrderContainer";
 export class ModUtils {
 
     get version(): string {
-        return '2.33.1';
+        return '2.33.2';
     }
 
     constructor(
